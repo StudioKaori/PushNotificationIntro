3bfc628e9c31c9ec231706f60c892ffefdd245a9
Studio Kaori <studiokaori.com@gmail.com>
2022-06-24 19:06:17 +0200

    Initial Commit

---
+++ PushNotificationIntro/PushNotificationIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationIntroApp.swift
+//  PushNotificationIntro
+//
+//  Created by Kaori Persson on 2022-06-24.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
